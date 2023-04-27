8a526538c4c2a7e11591ed144f7f71b4ffdb685c
Jagjeet Saini <jagjeetsaini@Jagjeets-MacBook-Air.local>
2023-04-27 14:52:38 +0530

    first commit

---
+++ first-pair.js
@@ -0,0 +1,8 @@
+/*  
+Questions for today 
+
+- To find a first pair from a number array whose sum is X
+X = 5
+arr = [1,2,3,4,5,6] // 2,3             */
+
+
---
+++ first-three-pair.js
@@ -0,0 +1,5 @@
+/*    
+- To find a first three elements from a number array whose sum is X
+X = 6
+arr = [1,2,3,4,5,6] // 1,2,3         */
+
